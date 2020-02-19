243a36f014be9aa413697ad86fd74bf297767ffa
TasanaK <tasana.kostic@gmail.com>
2020-02-19 21:18:53 +0100

    first commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,16 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("HelloWorld!!!!!");
+		System.out.print("Testing");
+		System.out.print(" app");
+		System.out.println(" with Selenium.");
+		System.out.println("Testing app with Selenium");
+		System.out.println("Save with CTRL+S...");
+		System.out.println(123456);
+	}
+
+}
